feb45ea000021c6daa96a9407bf33e678343a41b
Dearie <eburujacintha@gmail.com>
2026-02-07 22:00:06 +0100

    fixed welcome bug

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,22 @@
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import 'react-native-reanimated';
+import "../../global.css";
+
+
+const Layout = () => {
+  
+
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="welcome" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+      </Stack>
+      
+    </>
+  );
+}
+
+export default Layout;
