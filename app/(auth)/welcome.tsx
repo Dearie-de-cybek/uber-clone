e2fc0738c5cd64251a50667a412275bad376f623
Dearie <eburujacintha@gmail.com>
2026-02-07 22:20:23 +0100

    bug noticed for tailwind

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,10 +1,17 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import {router } from "expo-router";
 
 const Onboarding = () => {
   return (
-    <SafeAreaView>
-      <Text>Welcome</Text>
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'white', height: '100%', alignItems: 'center', justifyContent: 'space-between', }}>
+      <TouchableOpacity onPress={() => {
+        router.replace('/(auth)/sign-up')
+      }}
+      className="w-full flex justify-end items-end p-5 bg-blue-700"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
