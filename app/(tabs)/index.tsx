519769f1920b7c3fcfbe37a563230501a813d258
Dearie <eburujacintha@gmail.com>
2026-02-07 19:48:33 +0100

    fixed reanimated error

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,10 @@
-import { Platform, StyleSheet, View, Text, StatusBar } from 'react-native';
-import { SafeAreaView} from "react-native-safe-area-context";
-
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-   <SafeAreaView className="flex-1 items-center justify-center bg-blue-300">
-     <Text> Open up</Text>
-   </SafeAreaView>
+    <SafeAreaView className="flex-1 items-center justify-center bg-blue-500">
+      <Text> Open up</Text>
+    </SafeAreaView>
   );
 }
-
